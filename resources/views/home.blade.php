ef45e87afedea7acc3e1f929223904f5c3124082
giuliamari04 <giuliamari04@gmail.com>
2023-12-18 15:28:13 +0100

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
 <section>
-    <div class=" text-center py-2 my-tag" >current series</div>
-<main class="my-bg-primary d-flex  align-items-center "  id="card-content" >
+    <div class=" text-center py-2 my-tag position-absolute " >current series</div>
+<div  id="card-content" >
    <div class="container" >
     <div class="row py-5">
         @foreach ( $comics as $comic )
@@ -19,7 +19,62 @@
 
     </div>
    </div>
-  </main>
+   <div class="text-center">
+<button class=" text-center my-tag py-2 border-0 mb-3" >load more</button>
+   </div>
+
+  </div>
+
 </section>
+<section id="buy-comics">
+    <div class="container text-center py-4">
+    <div class="d-inline-flex flex-row my-w d-flex justify-content-center align-items-center">
+      <div class="d-flex align-items-baseline text-uppercase text-light  px-3">
+        <div  class="icon ">
+          <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="icon" class="w-100" />
+        </div>
+        <div class="w-75 fw-light">
+          <span>digital comics</span>
+        </div>
+      </div>
+
+      <div class="d-flex align-items-baseline text-uppercase text-light  px-3">
+        <div  class="icon ">
+          <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="icon" class="w-100" />
+        </div>
+        <div class="w-75 fw-light">
+          <span>dc merchandise</span>
+        </div>
+      </div>
+
+      <div class="d-flex align-items-baseline text-uppercase text-light  px-3">
+        <div  class="icon ">
+          <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="icon" class="w-100" />
+        </div>
+        <div class="w-75 fw-light">
+          <span>subscribtion</span>
+        </div>
+      </div>
+
+      <div class="d-flex align-items-baseline text-uppercase text-light  px-3">
+        <div  class="icon ">
+          <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="icon" class="w-100" />
+        </div>
+        <div class="w-75 fw-light">
+          <span>comic shop locator</span>
+        </div>
+      </div>
+
+      <div class="d-flex align-items-baseline text-uppercase text-light  px-3">
+        <div  class="icon ">
+          <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="icon" class="w-100" />
+        </div>
+        <div class="w-75 fw-light">
+          <span>dc power visa</span>
+        </div>
+      </div>
+    </div>
+</div>
+  </section>
 
 @endsection
