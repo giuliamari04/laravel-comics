be69bd207861c2eb33109a0db0acf4e30a283514
giuliamari04 <giuliamari04@gmail.com>
2023-12-20 09:35:42 +0100

    correzione

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -11,7 +11,7 @@
             <div class="col-12 col-md-4 col-lg-2">
                 <div class="card bg-trasparent text-light border-1 border-light h-100">
                     <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" class="card-img-top">
-                    <div class="card-body">
+                    <div class="card-body d-flex flex-column justify-content-between ">
                         <h5 class="card-title">{{$comic['title']}}</h5>
                         <p class="card-text">{!! substr($comic['description'], 0, 100) . '...' !!}</p>
                         <a href="{{route('comics.show', $key)}}" class="btn btn-primary">Vedi dettaglio</a>
